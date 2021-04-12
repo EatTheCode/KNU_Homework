4f8ae876d17061d34fa2f6fcebf9f77138ecde8c
wndudghks <63054541+wndudghks@users.noreply.github.com>
2021-04-13 03:06:13 +0900

    Add files via upload

---
+++ test/item/bad.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame.rect import *
+import random
+class badItem:
+    def __init__(self):
+        self.item=pygame.image.load('coronavirus.png')
+        self.item=pygame.transform.scale(self.item,(50,50))
+        self.recItem =self.item.get_rect()
+
+    def setSpeed(self):
+        return 1.3
+
+    def moveItem(self,SCREEN,SCREEN_WIDTH,SCREEN_HEIGHT):
+        self.recItem.x-=2
+        if  self.recItem.x<0:
+             self.recItem.x=SCREEN_WIDTH
+             self.recItem.y=random.randint(0,SCREEN_HEIGHT)
+        SCREEN.blit(self.item, self.recItem)
+
+    def checkCollision(self,recPlayer):
+        if self.recItem.top<recPlayer.bottom \
+            and recPlayer.top<self.recItem.bottom\
+            and self.recItem.left<recPlayer.right\
+            and recPlayer.left<self.recItem.right:
+            return True
